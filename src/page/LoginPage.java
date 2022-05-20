9964ae6fea0c40dde8fe98eef2027b936a7d5dd6
sudeer9 <43990469+sudeer9@users.noreply.github.com>
2022-05-20 18:08:05 +0530

    Uploading automation framework

---
+++ src/page/LoginPage.java
@@ -0,0 +1,55 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+public class LoginPage 
+{
+	@FindBy(id="username")
+	private WebElement unTB;
+	@FindBy(name="pwd")
+	private WebElement pwTB;
+	@FindBy(xpath="//div[.='Login ']")
+	private WebElement loginBTN;
+	@FindBy(xpath="//span[contains(.,'invalid')]")
+	private WebElement errorMSG;
+
+	public LoginPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	//Methods
+	
+	public void login(String un, String pw) throws InterruptedException
+	{
+		unTB.sendKeys(un);
+		pwTB.sendKeys(pw);
+		loginBTN.click();
+		Thread.sleep(4000);
+	}
+	
+	/*
+	public void setuserName(String un)
+	{
+		unTB.sendKeys(un);
+	}
+	public void setPassword(String pw)
+	{
+		pwTB.sendKeys(pw);
+	}
+	public void clickLogin()
+	{
+		loginBTN.click();
+	}
+	
+	*/
+	public void verifyErrorMSG(String expectedMSG)
+	{
+		//
+		String actualMSG = errorMSG.getText();
+		Assert.assertEquals(actualMSG, expectedMSG);
+	}
+}
